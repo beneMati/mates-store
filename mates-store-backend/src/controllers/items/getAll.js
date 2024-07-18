d8d7a0e4e68e63b469e833ddc309f1026423c671
beneMati <benedettomatias1991@gmail.com>
2024-07-18 19:46:45 -0300

    Debug log added

--- mates-store-backend/src/controllers/items/getAll.js
+++ mates-store-backend/src/controllers/items/getAll.js
@@ -3,7 +3,9 @@ const sendResponse = require("../../utils/handleResponse")
 
 const getItems = async (req, res) => {
 	req.log.info("Start transaction getItems")
+	req.log.debug("Begin Items Service")
 	const data = await itemsService.getAll()
+	req.log.debug("Stop Items Service")
 	sendResponse(res, 200, "Success", data)
 	req.log.info("End transaction getItems")
 }
